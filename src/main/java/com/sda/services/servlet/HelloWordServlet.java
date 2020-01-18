4109be11872a3c3b8c6caf5cd3b5898176096dfd
Olga Wojtaszko <olga.jackowska@wp.pl>
2020-01-18 13:05:02 +0100

    Zadanie 20 Aplikacja webowa HelloWord wykorzystująca servlety + konfiguracja pliku web.xml

---
+++ src/main/java/com/sda/services/servlet/HelloWordServlet.java
@@ -0,0 +1,18 @@
+package com.sda.services.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name="HelloWordServlet", urlPatterns = "/")
+
+public class HelloWordServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        PrintWriter writer = response.getWriter();
+        writer.println("Hello word!");
+    }
+}
